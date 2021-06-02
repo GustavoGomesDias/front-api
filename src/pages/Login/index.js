59cd0cea093ccde05786af543366b97c84fb74f6
GustavoGomesDias <treeelevengg@gmail.com>
2021-06-02 01:20:50 -0300

    feat: add styled components (and examples)

---
+++ src/pages/Login/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container } from '../../styles/GlobalStyles';
+import { Title } from './styled';
+
+export default function Login() {
+  return (
+    <Container>
+      <Title isRed>
+        Login
+        <small>Oie</small>
+      </Title>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </Container>
+  );
+}
---
+++ src/pages/Login/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  background: green;
+
+  small {
+    font-size: 12px;
+    margin-left: 15px;
+    color: #999;
+  }
+`;
+
+// Vai ter o mesmo valor de p (html)
+export const Paragrafo = styled.p``;
