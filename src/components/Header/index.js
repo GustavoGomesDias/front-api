6d156a45cb3098a996bb2c05995faccd1b754513
GustavoGomesDias <treeelevengg@gmail.com>
2021-06-02 01:54:55 -0300

    chore: add react icons

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import { Nav } from './styled';
+
+export default function Header() {
+  return (
+    <Nav>
+      <a href="#">Home</a>
+      <a href="#">teste</a>
+    </Nav>
+  );
+}
