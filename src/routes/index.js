3d76960d33df674f9bb652f92fa1ef65f16f48ea
GustavoGomesDias <treeelevengg@gmail.com>
2021-06-03 00:37:59 -0300

    feat: add auth in routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
+import MyRoute from './MyRoute';
 import Login from '../pages/Login';
 import Page404 from '../pages/Page404';
 
@@ -13,8 +14,8 @@ import Page404 from '../pages/Page404';
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path="/" component={Login} />
-      <Route path="*" component={Page404} />
+      <MyRoute exact path="/" component={Login} isClosed />
+      <MyRoute path="*" component={Page404} />
     </Switch>
   );
 }
