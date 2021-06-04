009241187f4fe3e2659806ce5bc0c09fe50b0bc1
GustavoGomesDias <treeelevengg@gmail.com>
2021-06-04 01:50:38 -0300

    chore: add example to use axios in the project

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,6 @@ module.exports = {
     'import/prefer-default-export': 0,
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-    // 'react/react-in-jsx-scope': 'off',
+    'no-console': 0,
   },
 };
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Title } from './styled';
+import axios from '../../services/axios';
 
 export default function Login() {
+  // Como o array tá vazio (ele guarda as variaveis que vão mudar de estado), significa que o useEffect vai ser usado apenas uma vez
+  React.useEffect(() => {
+    // Não pode usar async com useEffect, por isso criamos uma função
+    async function getData() {
+      const { data } = await axios.get('/alunos');
+      console.log(data);
+    }
+
+    getData();
+  }, []);
+
   return (
     <Container>
       <Title isRed>
