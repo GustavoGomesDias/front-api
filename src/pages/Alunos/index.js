6f94702238c933785ed2c3a35c86891205aba1d3
GustavoGomesDias <treeelevengg@gmail.com>
2021-06-11 20:40:42 -0300

    feat: add alunos screen

--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { get } from 'lodash';
+import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa';
 
 import { Container } from '../../styles/GlobalStyles';
+import { AlunoContainer, ProfilePicture } from './styled';
+import axios from '../../services/axios';
 
 export default function Alunos() {
+  const [alunos, setAlunos] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get('/alunos');
+      setAlunos(response.data.alunos);
+    }
+
+    getData();
+  }, []);
+
   return (
     <Container>
       <h1>Alunos</h1>
+      <AlunoContainer>
+        {alunos.map((aluno) => (
+          <div hey={String(aluno.id)}>
+            <ProfilePicture>
+              {get(aluno, 'Images[0].url', false) ? (
+                <img src={aluno.Images[0].url} alt="" />
+              ) : (
+                <FaUserCircle size={36} />
+              )}
+            </ProfilePicture>
+
+            <span>{aluno.nome}</span>
+            <span>{aluno.email}</span>
+
+            <Link to={`/alunos/${aluno.id}/edit`}>
+              <FaEdit size={16} />
+            </Link>
+            <Link to={`/alunos/${aluno.id}/delete`}>
+              <FaWindowClose size={16} />
+            </Link>
+          </div>
+        ))}
+      </AlunoContainer>
     </Container>
   );
 }
--- src/pages/Alunos/styled.js
+++ src/pages/Alunos/styled.js
@@ -1,4 +1,23 @@
 import styled from 'styled-components';
 
-// Vai ter o mesmo valor de p (html)
-export const Paragrafo = styled.p``;
+export const AlunoContainer = styled.div`
+  margin-top: 20px;
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5px 0;
+  }
+
+  div + div {
+    border-top: 1px solid black;
+  }
+`;
+
+export const ProfilePicture = styled.div`
+  img {
+    width: 30px;
+    height: 36px;
+    border-radius: 50%;
+  }
+`;
